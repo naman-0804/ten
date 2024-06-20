c79fd7006f632a29dc54e5309059abc6a6a06a06
Naman Srivastava <110972759+un-identified@users.noreply.github.com>
2024-06-20 16:18:36 +0530

    Update app.py

--- api/app.py
+++ api/app.py
@@ -862,4 +862,4 @@ def reset_password():
 #     return jsonify({'message': 'Password reset email sent successfully'}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True, host='localhost', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
